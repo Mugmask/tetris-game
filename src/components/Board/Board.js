1a7a50cf04cc949ece85a44f2307a88dcacdb028
Francisco Perez Ugalde <franciscoperezugalde@gmail.com>
2024-06-11 20:10:48 -0300

    colisiones con bloques

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,12 +1,15 @@
 import "./Board.css";
 import Shapes from "../Shapes/Shapes";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 export function Board({ rows, columns, gameStatus }) {
   const [board, setBoard] = useState(Array.from({ length: rows }, () => Array.from({ length: columns }, () => 0)));
-  const [randomShape, setRandomShape] = useState(null);
+  const [randomShape, setRandomShape] = useState(Shapes[Math.floor(Math.random() * Shapes.length)]);
   const [position, setPosition] = useState({ x: 3, y: 0 });
-  const [newPosition, setNewPosition] = useState({ x: 3, y: 0 });
+
+  const positionRef = useRef(position);
+  const randomShapeRef = useRef(randomShape);
+  const boardRef = useRef(board);
 
   /*   TODO:
  - Mover piezas con el teclado
@@ -14,36 +17,14 @@ export function Board({ rows, columns, gameStatus }) {
  - fixear cambio de colores por pieza
  - */
   const createBoard = () => {
-    if (!board) return;
+    if (!boardRef.current) return;
 
-    let newBoard = board.map((row) => [...row]);
+    let newBoard = boardRef.current.map((row) => [...row]);
     return newBoard;
   };
-
-  window.addEventListener("keydown", (event) => {
-    setNewPosition({ x: position.x, y: position.y + 1 });
-    event.preventDefault();
-
-    let newBoard = createBoard();
-
-    if (event.key === "ArrowDown") {
-      if (!checkColision(newBoard, newPosition)) {
-        fillBoard(newBoard, newPosition);
-      }
-    }
-    if (event.key === "ArrowUp") {
-    }
-    if (event.key === "ArrowLeft") {
-    }
-    if (event.key === "ArrowRight") {
-    }
-  });
-
-  const draw = (startY, startX) => {
-    const newShape = Shapes[Math.floor(Math.random() * Shapes.length)];
-    setRandomShape(newShape);
-    let newBoard = board.map((row) => row.map((x) => x));
-    newShape.shape.forEach((row, rowIndex) => {
+  const draw = (currentShape, startY, startX) => {
+    let newBoard = boardRef.current.map((row) => row.map((x) => x));
+    currentShape.shape.forEach((row, rowIndex) => {
       row.forEach((cell, colIndex) => {
         if (cell) {
           newBoard[startY + rowIndex][startX + colIndex] = cell;
@@ -52,98 +33,134 @@ export function Board({ rows, columns, gameStatus }) {
     });
     setBoard(newBoard);
   };
-  const fillBoard = (newBoard, newPosition) => {
-    if (!randomShape) return;
+  const cleanPreviousPosition = (currentShape, newBoard) => {
+    // Limpiar la posición de la pieza anterior
+    currentShape.shape.forEach((row, rowIndex) => {
+      row.forEach((cell, colIndex) => {
+        if (cell) {
+          // Si la celda en el tablero original está ocupada, limpiarla
+          if (
+            boardRef.current[rowIndex + positionRef.current.y] &&
+            boardRef.current[rowIndex + positionRef.current.y][colIndex + positionRef.current.x]
+          ) {
+            newBoard[rowIndex + positionRef.current.y][colIndex + positionRef.current.x] = 0;
+          }
+        }
+      });
+    });
+    return newBoard;
+  };
+  const moveShape = (newPosition) => {
+    if (!randomShapeRef.current) return;
 
-    randomShape.shape.forEach((row, rowIndex) => {
+    let newBoard = createBoard();
+    let currentShape = randomShapeRef.current;
+
+    newBoard = cleanPreviousPosition(currentShape, newBoard);
+
+    const collisionResult = checkColision(newBoard, newPosition);
+
+    if (collisionResult.type !== "none") {
+      if (collisionResult.type === "wall") {
+        return;
+      } else if (collisionResult.type == "block" && positionRef.current.y == 0) {
+        if (boardRef.current[0].find((x) => x == 1)) gameStatus("gameOver");
+        return;
+      } else {
+        setBoard(newBoard);
+        setPosition({ x: 3, y: 0 });
+        const newShape = Shapes[Math.floor(Math.random() * Shapes.length)];
+        setRandomShape(newShape);
+        draw(newShape, 0, 3);
+      }
+      return;
+    }
+
+    currentShape.shape.forEach((row, rowIndex) => {
       row.forEach((cell, colIndex) => {
         if (cell) {
           newBoard[rowIndex + newPosition.y][colIndex + newPosition.x] = cell;
         }
       });
     });
-
     setBoard(newBoard);
-    setPosition({ x: newPosition.x, y: newPosition.y });
-  };
-  const moveShape = () => {
-    if (!randomShape) return;
-
-    let newBoard = createBoard();
-
-    // Limpiar la posición de la pieza anterior
-    randomShape.shape.forEach((row, rowIndex) => {
-      row.forEach((cell, colIndex) => {
-        if (cell) {
-          // Si la celda en el tablero original está ocupada, limpiarla
-          if (board[rowIndex + position.y] && board[rowIndex + position.y][colIndex + position.x]) {
-            newBoard[rowIndex + position.y][colIndex + position.x] = 0;
-          }
-        }
-      });
-    });
-
-    //ir uno para abajo
-    let y = position.y + 1;
-    let x = position.x;
-
-    if (!checkColision(newBoard, { y, x })) {
-      randomShape.shape.forEach((row, rowIndex) => {
-        row.forEach((cell, colIndex) => {
-          if (cell) {
-            newBoard[rowIndex + y][colIndex + x] = cell;
-          }
-        });
-      });
-
-      setBoard(newBoard);
-      setPosition({ x: x, y: y });
-    } else {
-      if (y === 1) {
-        gameStatus("gameOver");
-        return;
-      }
-      /* setPosition({ x: 3, y: 0 });
-      draw(0, 3); */
-    }
+    setPosition(newPosition);
   };
   const checkColision = (newBoard, move) => {
-    if (!randomShape) return;
+    if (!randomShapeRef.current) return;
 
-    const shape = randomShape.shape;
-    //todo: hacer los posibles casos por el trigger
-    /*     const newY = position.y + 1;
-    let newX = position.x + 1; */
-    for (let rowIndex = 0; rowIndex < shape.length; rowIndex++) {
-      for (let colIndex = 0; colIndex < shape[rowIndex].length; colIndex++) {
-        if (shape[rowIndex][colIndex]) {
+    let currentShape = randomShapeRef.current?.shape;
+
+    for (let rowIndex = 0; rowIndex < currentShape.length; rowIndex++) {
+      for (let colIndex = 0; colIndex < currentShape[rowIndex].length; colIndex++) {
+        if (currentShape[rowIndex][colIndex]) {
           const newRow = move.y + rowIndex;
           const newCol = move.x + colIndex;
-
-          if (newRow >= rows || newCol < 0 || newCol >= columns || newBoard[newRow][newCol]) {
-            return true;
+          if (newCol >= columns || newCol < 0) {
+            return { type: "wall", position: { x: newCol, y: newRow } };
+          }
+          if (newRow >= rows || newBoard[newRow][newCol]) {
+            return { type: "block", position: { x: newCol, y: newRow } };
           }
         }
       }
     }
-    return false;
+    return { type: "none" };
   };
 
   useEffect(() => {
-    if (!randomShape) draw(0, 3);
+    draw(randomShapeRef.current, 0, 3);
+
+    const handleKeyDown = (event) => {
+      event.preventDefault();
+
+      let newPos = { ...positionRef.current };
+
+      if (event.key === "ArrowDown") {
+        newPos.y += 1;
+      }
+      if (event.key === "ArrowUp") {
+        // Maneja la lógica de ArrowUp
+      }
+      if (event.key === "ArrowLeft") {
+        newPos.x -= 1;
+      }
+      if (event.key === "ArrowRight") {
+        newPos.x += 1;
+      }
+
+      moveShape(newPos);
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
   }, []);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      moveShape();
+      moveShape({ ...positionRef.current, y: positionRef.current.y + 1 });
     }, 1000);
 
     return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    boardRef.current = board;
   }, [board]);
+  useEffect(() => {
+    positionRef.current = position;
+  }, [position]);
+
+  useEffect(() => {
+    randomShapeRef.current = randomShape;
+  }, [randomShape]);
 
   return (
     <div className="board-container">
-      {board.map((row, indexRow) => {
+      {boardRef.current.map((row, indexRow) => {
         return (
           <ul key={`${indexRow}`}>
             {row.map((column, indexCol) => {
